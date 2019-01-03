7d07c0259c250f6c6c360b93fd2fece7a0b7b626
Loic-Remy <loic.remy@hotmail.com>
2019-01-04 00:58:43 +0100

    Travail sur la reconnaissance des guillements
    Tests pour voir si fonctionne : TO DO

--- main.c
+++ main.c
@@ -16,16 +16,20 @@ cli(int bufSize, char *cpyBuffer, char ***tabPointer, size_t *nb)
 	char *pCursor=cpyBuffer;
 	char **tempPointer=NULL;
 	size_t i=0;
+	int gui=1;
 	
 	tempPointer=malloc(sizeof(char*)*3);
 	tempPointer[0]=pCursor;
 	
-	for (i=0; i<=strlen(cpyBuffer)-1; i++) {
-		if (strcmp(pCursor," ")==0) {
+	for (i=0; i<=bufSize-1; i++) {
+		if (*pCursor==32 && gui==1) {
 			*pCursor='\0';
 			pCursor++;
 			tempPointer[*nb]=pCursor;
 			(*nb)++;
+		}
+		else if (*pCursor==34) {
+			gui = gui*(-1);
 		}
 		pCursor++;
 	}
@@ -57,7 +61,7 @@ int main (int argc, char *argv[])
 	char **tabPointer=NULL;
 	size_t nb=1;
 
-	strcpy(buffer,"calc vs 23.11.18 24.11.18");
+	strcpy(buffer,"edit vs 27 \"Nouvel an\"");
 	
 	cli(BUF_SIZE,buffer,&tabPointer,&nb);
 
